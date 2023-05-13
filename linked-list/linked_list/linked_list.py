61265d8ae3b4a02cc3199543a4f27145def523c9
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-05-13 20:48:38 +0000

    i solved ch 5

---
+++ linked-list/linked_list/linked_list.py
@@ -0,0 +1,38 @@
+class Node :
+    def __init__(self,value,next_=None) :
+        self.value=value
+        self.next=next_
+    
+class linked_list:
+    def __init__(self,head=None) :
+       self.head=head
+    
+        
+    def insert (self,value):
+       
+       new_node =Node(value)
+       new_node.next=self.head
+       self.head= new_node
+    
+    def includes(self,value):
+        current=self.head
+        while current.value!=value:
+           current=current.next
+           if current==None:
+               return False
+        if current.value==value :
+            return True
+        
+    def to_string (self):
+        current=self.head
+        string=""
+        while current!=None:
+            string+= "{ "+f"{current}"" }"+" -> "
+            current=current.next
+        string+= " None "
+        return string
+            
+            
+    
+
+
