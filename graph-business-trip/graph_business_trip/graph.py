cbd5a7efc925e99ab986ad903b09f59ec6ff00be
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-08-14 23:42:58 +0300

    isolved lab27

--- graph-business-trip/graph_business_trip/graph.py
+++ graph-business-trip/graph_business_trip/graph.py
@@ -1,19 +1,4 @@
-'''
-- implement the graph with all methods needed.
-- vertex  *
-- Edge  *
-- graph *
-methods : 
-  - add_vertex() * 
-  - add_edge() *
-  - get_verteces() *
-  - get_neighbors() *
-  - size () *
-- breadth first traversal . 
-  - Queue() *
-'''
 from collections import deque 
-# we spill it 'd-e-c-k'
 
 class Queue:
 
@@ -29,132 +14,96 @@ class Queue:
     def __len__(self):
         return len(self.dq)
 
-
-
-class Vertix:
+class Vertex:
 
     def __init__(self, value):
-
         self.value = value
 
     def __str__(self):
-        return self.value
-
+        return str(self.value)
 
 class Edge:
 
-    def __init__(self, vertix, weight=0):
+    def __init__(self, vertex, weight=0):
         self.weight = weight
-        self.vertix = vertix
+        self.vertex = vertex
 
 class Graph:
 
     def __init__(self):
         self.__adj_list = {}
-      
-    def add_vertix(self,value):
-      '''
-      Arguments: value
-      Returns: The added vertex
-      Add a vertex to the graph
-      '''
-  
-      vertix = Vertix(value)
-      self.__adj_list[vertix] = []
-      return vertix
 
+    def add_Vertex(self, value):
+        vertex = Vertex(value)
+        self.__adj_list[vertex] = []
+        return vertex
 
-
-    def add_edge(self, start_vertix, end_vertix, weight=0):
-        '''
-        Arguments: 2 vertices to be connected by the                     edge, weight (optional)
-        Returns: nothing
-        Adds a new edge between two vertices in the graph
-        If specified, assign a weight to the edge
-        Both vertices should already be in the Graph
-        '''
-        if start_vertix not in self.__adj_list:
+    def add_edge(self, start_vertex, end_vertex, weight=0):
+        if start_vertex not in self.__adj_list:
             raise KeyError("Start vertex is not found")
-        if end_vertix not in self.__adj_list:
+        if end_vertex not in self.__adj_list:
             raise KeyError("End vertex is not found")
-        if start_vertix == end_vertix:
-            edge1 = Edge(end_vertix, weight)
-            self.__adj_list[start_vertix].append(edge1)
+        if start_vertex == end_vertex:
+            edge1 = Edge(end_vertex, weight)
+            self.__adj_list[start_vertex].append(edge1)
             return
-        edge1 = Edge(end_vertix, weight)
-        edge2 = Edge(start_vertix,weight)
-        self.__adj_list[start_vertix].append(edge1)
-        self.__adj_list[end_vertix].append(edge2)
+        edge1 = Edge(end_vertex, weight)
+        edge2 = Edge(start_vertex, weight)
+        self.__adj_list[start_vertex].append(edge1)
+        self.__adj_list[end_vertex].append(edge2)
 
-  
-  
     def get_vertices(self):
-      '''
-        Arguments: none
-        Returns all of the vertices in the graph as a  
-        collection (set, list, or similar)
-        Empty collection returned if there are no vertices
-      '''
-  
-      return self.__adj_list.keys()
+        return self.__adj_list.keys()
 
-  
     def size(self):
-      return len(self.__adj_list)
-  
-    def get_neighbors(self,vertix):
-      '''
-      get neighbors
-      A rguments: vertex
-      Returns a collection of edges connected to the 
-      given vertex
-      Include the weight of the connection in the                      returned collection
-      Empty collection returned if there are no vertices
-      '''
-      # return self.__adj_list[vertix]
-      return self.__adj_list.get(vertix, [])
-  
-  
-    def breadth_first(self,start_vertix):
-    
+        return len(self.__adj_list)
+
+    def get_neighbors(self, vertex):
+        return self.__adj_list.get(vertex, [])
+
+    def get_edge(self, vertex1, vertex2):
+        for edge in self.__adj_list[vertex1]:
+            if edge.vertex == vertex2:
+                return edge
+
+    def breadth_first(self, start_vertex):
         result = []
-        visted = set()
+        visited = set()
         q = Queue()
 
-        q.enqueue(start_vertix)
-        visted.add(start_vertix)
+        q.enqueue(start_vertex)
+        visited.add(start_vertex)
 
         while len(q):
-            current_vertix = q.dequeue()
+            current_vertex = q.dequeue()
 
-            result.append(current_vertix.value)
+            result.append(current_vertex.value)
 
-            neighbors =self.get_neighbors(current_vertix)
+            neighbors = self.get_neighbors(current_vertex)
 
             for edge in neighbors:
-                neighbor = edge.vertix
-                if neighbor not in visted:
+                neighbor = edge.vertex
+                if neighbor not in visited:
                     q.enqueue(neighbor)
-                    visted.add(neighbor)
+                    visited.add(neighbor)
 
         return result
-                   
-            
+
+
 if __name__ == "__main__":
     g = Graph()
-    a = g.add_vertix('A')
-    b = g.add_vertix('B')
-    e = g.add_vertix('E')
-    c = g.add_vertix('C')
-    d = g.add_vertix('D')
+    a = g.add_Vertex('A')
+    b = g.add_Vertex('B')
+    e = g.add_Vertex('E')
+    c = g.add_Vertex('C')
+    d = g.add_Vertex('D')
 
-    g.add_edge(a,b)
-    g.add_edge(a,c)
-    g.add_edge(b,d)
-    g.add_edge(b,e)
-    g.add_edge(e,d)
-    g.add_edge(e,c)
-    print(g.breadth_first(a))
-
-    
-               
+    g.add_edge(a, b)
+    g.add_edge(a, c)
+    g.add_edge(b, d)
+    g.add_edge(b, e)
+    g.add_edge(e, d)
+    g.add_edge(e, c)
+    vertices = g.get_vertices()
+    vertex_names = [vertex.value for vertex in vertices]
+    print(vertex_names)
--- graph-business-trip/graph_business_trip/graph_business_trip.py
+++ graph-business-trip/graph_business_trip/graph_business_trip.py
@@ -1,16 +1,46 @@
-from graph import Vertix, Edge, Graph, Queue
+from graph import Graph, Vertex, Edge, Queue
+
+
 def business_trip(graph, city_names):
-    if 
+    if not city_names:
+        return None
+
+    total_cost = 0
+    vertices = graph.get_vertices()
+    for i in range(len(city_names) - 1):
+        current_city = city_names[i]
+        next_city = city_names[i + 1]
+
+        current_vertex = None
+        next_vertex = None
+
+        for vertex in vertices:
+            if vertex.value == current_city:
+                current_vertex = vertex
+            if vertex.value == next_city:
+                next_vertex = vertex
+
+        if not current_vertex or not next_vertex:
+            return None  # One of the cities is not in the graph
+
+        edge = graph.get_edge(current_vertex, next_vertex)
+        if not edge:
+            return None  # There's no direct connection between cities
+
+        total_cost += edge.weight
+
+    return total_cost
+
 
 # Example graph setup
 if __name__ == "__main__":
     g = Graph()
-    metroville = g.add_vertix('Metroville')
-    pandora = g.add_vertix('Pandora')
-    arendelle = g.add_vertix('Arendelle')
-    new_monstropolis = g.add_vertix('New Monstropolis')
-    naboo = g.add_vertix('Naboo')
-    narnia = g.add_vertix('Narnia')
+    metroville = g.add_Vertex('Metroville')
+    pandora = g.add_Vertex('Pandora')
+    arendelle = g.add_Vertex('Arendelle')
+    new_monstropolis = g.add_Vertex('New Monstropolis')
+    naboo = g.add_Vertex('Naboo')
+    narnia = g.add_Vertex('Narnia')
 
     g.add_edge(metroville, pandora, 82)
     g.add_edge(arendelle, new_monstropolis, 115)
@@ -20,8 +50,8 @@ if __name__ == "__main__":
     test_cases = [
         [metroville, pandora],  # Expected output: $82
         [arendelle, new_monstropolis, naboo],  # Expected output: $115
-        [naboo, pandora],  # Expected output: null
-        [narnia, arendelle, naboo]  # Expected output: null
+        [naboo, pandora],  # Expected output: None
+        [narnia, arendelle, naboo]  # Expected output: None
     ]
     
     for cities in test_cases:
